4c752fec40d872ce993d1e069c79b8eb3faccfb0
Gowtham Raj <gowthamrajlegend@gmail.com>
2023-07-30 00:46:40 +0530

    notes

---
+++ web/front/js/script.js
@@ -0,0 +1,33 @@
+//RULES
+/*
+rules 
+1 not use keyword names 
+2 not start with number,spaces,ifen 
+3 case sebnsitive 
+4 use meaning full words
+*/
+
+//calling variable - var, let, const
+//var name = "raj";
+//const dob = "18/12/2003"
+//let msg = "hi i am " + name + " ok" +dob;
+
+//alert box
+//alert(msg)
+
+// console.log
+//console.log(msg);
+
+//DATA TYPES
+//primitive Types
+let name = "ravi"; //string 
+let age = 18; //number
+let isyouth = true; //boolean
+let x; //undefined
+let y = null; //null
+
+//Non Primitive Types or Reference Types
+let a = {}; //object
+let b = []; //array
+let c = function () {}; //function
+
